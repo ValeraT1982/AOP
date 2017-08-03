e12f129432f01f839c2cb264333a85d0a85c9cd3
valerat1982 <tereschenkoVV@gmail.com>
2017-08-02 17:54:54 -0700

    Start

---
+++ AOP/AOP.Tests/CurrentThreadTaskScheduler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AOP.Tests
+{
+    public class CurrentThreadTaskScheduler: TaskScheduler
+    {
+        protected override IEnumerable<Task> GetScheduledTasks()
+        {
+            return Enumerable.Empty<Task>();
+        }
+
+        protected override void QueueTask(Task task)
+        {
+            TryExecuteTask(task);
+        }
+
+        protected override bool TryExecuteTaskInline(Task task, bool taskWasPreviouslyQueued)
+        {
+            TryExecuteTask(task);
+            return true;
+        }
+    }
+}
